304d888c4cef058d5f278c303117cc5782d3642a
Alexander Navarro <ale.navarro.parra@gmail.com>
2020-10-24 09:59:09 -0300

    bashrc added

--- installation/install.sh
+++ installation/install.sh
@@ -3,8 +3,12 @@
 packages='packages.txt'
 n=1
 
+# read package file and install everythin, eather from the oficial repository or user repository
 while read line; do
-   sudo pacman -S --noconfirm --needed "$line" || yay -S "$line" <dev/tty
+   sudo pacman -S --noconfirm --needed "$line" || yay -S "$line" </dev/tty
 done < $packages
 
 echo "Insallet packages"
+
+# Storage github user and passwor so don't ask every time
+git config --global credential.helper store
