8d54bfdcd5c3b255c476cd030e5e0b173062e7d5
Alexander Navarro <ale.navarro.parra@gmail.com>
2021-05-25 17:14:23 -0400

    update 05-25-2021

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -9,39 +9,59 @@ package.loaded['awful.hotkeys_popup.keys.tmux'] = {}
 
 -- local auto_start = true
 
--- ***** Variables ***** --
 
 -- ##### User Variables ##### --
 
+--[[
+Return the terminal as a string.
+@tparam app string The aplication to open in the terminal.
+@tparam class string The class to give to the terminal.
+@treturn string The formated string with the terminal, class and aplication to run with awful.spawn_with_shell().
+--]]
+local function terminal(app, class)
+    local terminal = "kitty -1"
+
+    if class then terminal = terminal .. " --class " .. class end
+    if app then terminal = terminal .. " -e " .. app end
+
+    return terminal
+end
+
 USER = {
-    profile_picture = os.getenv("HOME").."/.config/awesome/themes/user.jpg",
-    terminal = "kitty -1",
+    profile_picture         =       os.getenv("HOME").."/.config/awesome/themes/user.jpg",
+    terminal                =       terminal(),
 
     -- Rofi script to open terminal in custom path
-    terminal_selector = os.getenv("HOME") .. "/dotfiles/scripts/rofi/open_terminal.sh",
+    terminal_selector       =       os.getenv("HOME") .. "/dotfiles/scripts/rofi/open_terminal.sh",
 
-    browser = os.getenv("HOME") .. "/dotfiles/scripts/rofi/rofi_buku",
-    file_manager = "thunar",
-    file_manager_cli = "kitty -1 --class files -e ranger",
-    editor = "code",
-    editor_cli = "kitty -1 --class editor -e nvim",
+    browser                 =       os.getenv("HOME") .. "/dotfiles/scripts/rofi/rofi_buku",
+    file_manager            =       "thunar",
+    file_manager_cli        =       terminal("ranger", "files"),
+    editor                  =       "code",
+    editor_cli              =       terminal("nvim", "editor"),
 
     -- Rofi script to open the editor in custom path
-    editor_selector = os.getenv("HOME") .. "/dotfiles/scripts/rofi/edit_file.sh",
+    editor_selector         =       os.getenv("HOME") .. "/dotfiles/scripts/rofi/edit_file.sh",
 
-    screenshot = "sleep 0.5 && scrot -ub /tmp/screenshot-$(date +%F_%T).png -e 'xclip -selection c -t image/png < $f'",
-    screenshot_gui = "flameshot gui",
+    screenshot              =       "sleep 0.5 && scrot -ub /tmp/screenshot-$(date +%F_%T).png -e 'xclip -selection c -t image/png < $f'",
+    screenshot_gui          =       "flameshot gui",
 
     -- Launchers --
-    app_launcher = "~/.config/rofi/launchers/misc/launcher.sh",
+    app_launcher            =       "~/.config/rofi/launchers/misc/launcher.sh",
 
-    mpd = "kitty -1 --class music -e ncmpcpp",
-    mpd_server = "10.0.0.10",
+    mpd                     =       terminal("ncmpcpp", "music"),
+    mpd_server              =       "10.0.0.10",
 
-    powermenu = os.getenv("HOME") .. "/dotfiles/config/rofi/applets/menu/powermenu.sh",
-    run_command = os.getenv("HOME") .. "/dotfiles/scripts/rofi/exec_command",
-    open_bookmark = os.getenv("HOME") .. "/dotfiles/scripts/rofi/buku",
-    password_manager = "bwmenu",
+    powermenu               =       os.getenv("HOME") .. "/dotfiles/config/rofi/applets/menu/powermenu.sh",
+    run_command             =       os.getenv("HOME") .. "/dotfiles/scripts/rofi/exec_command",
+    open_bookmark           =       os.getenv("HOME") .. "/dotfiles/scripts/rofi/buku",
+    password_manager        =       "bwmenu",
+
+    -- >> Battery <<
+    -- You will receive notifications when your battery reaches these
+    -- levels.
+    battery_threshold_low = 20,
+    battery_threshold_critical = 5,
 
     dirs = {
         downloads = os.getenv("XDG_DOWNLOAD_DIR") or "~/Downloads",
@@ -53,32 +73,60 @@ USER = {
         -- are not lost
         --     screenshots = os.getenv("XDG_SCREENSHOTS_DIR") or "~/Pictures/Screenshots",
     },
-    sidebar = {
-        hide_on_mouse_leave = true,
-        show_on_mouse_screen_edge = true,
-    },
+}
 
-    -- >> Lock screen <<
-    -- This password will ONLY be used if you have not installed
-    -- https://github.com/RMTT/lua-pam
-    -- as described in the README instructions
-    -- Leave it empty in order to unlock with just the Enter key.
-    -- lock_screen_custom_password = "",
-    lock_screen_custom_password = "awesome",
-
-    -- >> Battery <<
-    -- You will receive notifications when your battery reaches these
-    -- levels.
-    battery_threshold_low = 20,
-    battery_threshold_critical = 5,
-
+local auto_start_apps = {
+    "telegram-desktop",
+    "whatsapp-nativefier",
+    "discord",
+    USER.mpd,
 }
 
 awful.util.shell = "/usr/bin/bash"
 
--- ##### User variables ##### --
-
 -- ##### Themes ##### --
+
+require("awful.autofocus")
+
+-- Load theme, see themes directory for a list of themes
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/default.lua")
+
+-- ##### Custom Widgets and Stuff ##### --
+
+-- Status bar(s)
+require("custom_objects.bars.default")
+
+
+-- ##### Wallpaper ##### --
+-- In case you need it, if you don't manage the wallpaper by your own
+
+-- local function set_wallpaper(s)
+-- Wallpaper
+-- if beautiful.wallpaper then
+-- local wallpaper = beautiful.wallpaper
+-- -- If wallpaper is a function, call it with the screen
+-- if type(wallpaper) == "function" then
+--     wallpaper = wallpaper(s)
+-- end
+
+-- >> Method 1: Built in wallpaper function
+gears.wallpaper.fit(beautiful.wallpaper, s)
+gears.wallpaper.maximized(beautiful.wallpaper, s)
+
+-- >> Method 2: Set theme's wallpaper with feh
+--awful.spawn.with_shell("feh --bg-fill " .. wallpaper)
+
+-- >> Method 3: Set last wallpaper with feh
+-- awful.spawn.with_shell(os.getenv("HOME") .. "/.fehbg")
+-- end
+-- end
+-- Set wallpaper (I do it via script)
+-- awful.screen.connect_for_each_screen(function(s)
+--     -- Wallpaper
+--     set_wallpaper(s)
+-- end)
+
+-- ##### Tags ##### --
 
 awful.layout.layouts = {
     awful.layout.suit.spiral.dwindle,
@@ -98,60 +146,6 @@ awful.layout.layouts = {
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
 }
-
--- ##### Theme handling ##### --
-
--- Load AwesomeWM libraries
-require("awful.autofocus")
-
--- Load theme, see themes directory for a list of themes
-beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/default.lua")
-
--- ##### Features ##### --
-
--- Desktop Components --
-
--- Status bar(s)
-require("custom_objects.bars.default")
-
--- -- Daemons --
--- require("daemons_utils")
-
--- ================================================== --
--- Layouts
-awful.layout.layout = layouts
-
--- Wallpaper
--- ===================================================================
-local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        -- local wallpaper = beautiful.wallpaper
-        -- -- If wallpaper is a function, call it with the screen
-        -- if type(wallpaper) == "function" then
-        --     wallpaper = wallpaper(s)
-        -- end
-
-        -- >> Method 1: Built in wallpaper function
-        gears.wallpaper.fit(beautiful.wallpaper, s)
-        gears.wallpaper.maximized(beautiful.wallpaper, s)
-
-        -- >> Method 2: Set theme's wallpaper with feh
-        --awful.spawn.with_shell("feh --bg-fill " .. wallpaper)
-
-        -- >> Method 3: Set last wallpaper with feh
-        -- awful.spawn.with_shell(os.getenv("HOME") .. "/.fehbg")
-    end
-end
--- Set wallpaper (I do it via script)
--- awful.screen.connect_for_each_screen(function(s)
---     -- Wallpaper
---     set_wallpaper(s)
--- end)
-
--- ##### Features ##### -- }}}
-
--- ##### Tags ##### {{{
 
 awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table
@@ -198,20 +192,7 @@ awful.screen.connect_for_each_screen(function(s)
     -- ...
 end)
 
--- ##### Tags #####
-
-require("config.rules")
-
--- ##### Autostart #####
-
-
-auto_start_apps = {
-    os.getenv("HOME") .. "/dotfiles/scripts/master.sh",
-    "telegram-desktop",
-    "whatsapp-nativefier",
-    "discord",
-    USER.mpd,
-}
+-- ##### Autostart Apps #####
 
 if auto_start then
     for app = 1, #auto_start_apps do
@@ -219,9 +200,7 @@ if auto_start then
     end
 end
 
--- ##### Autostart ##### }}}
-
--- ##### Error Handling ##### -- {{{
+-- ##### Error Handling ##### --
 
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
@@ -244,10 +223,10 @@ do
     end)
 end
 
--- ##### Error Handling ##### -- }}}
+-- ##### General Config ##### --
 
+require("config.rules")
 require("config.signals")
 
--- ***** Initialization ***** -- }}}
 
 -- vim:foldmethod=syntax
--- scripts/set_wallpapers.sh
+++ scripts/set_wallpapers.sh
@@ -1,14 +1,43 @@
 #!/usr/bin/env bash
 
+
+result_size=20
+
 source "$HOME/dotfiles/scripts/env_variables.sh"
+dependencies=(vimiv)
 
-# Get wallpapers and convert the string output in array
-# This works even if the path of some wallpapers contain a space
-output=$(find "$wallpapers" -path "*Fondos_PC*" | shuf -n 5)
-readarray -t finded_wallpapers <<<"$output"
+for dep in "${dependencies[@]}"; do
+    if ! command -v "$dep" &> /dev/null; then
+        echo -e "$red${dep}$reset not fount!"
+        exit 1
+    fi
+done
 
-# Set wallpapers
-feh --bg-fill "${finded_wallpapers[@]}"
+finish="n"
+
+while true; do
+    # Get wallpapers and convert the string output in array
+    # This works even if the path of some wallpapers contain a space
+    output=$(find "$wallpapers" -path "*Fondos_PC*" | shuf -n "$result_size")
+    readarray -t finded_wallpapers <<<"$output"
+
+    # Open the random wallpapers for selection
+    # vimiv handle path with space itself.
+
+    echo -e "Select at least 1 wallpaper to use
+    The first wallpaper will set the color scheme and be set in the primary monitor (if you have more than one)"
+
+    selected_wallpapers=($(vimiv --command "enter thumbnail" -o %m --set thumbnail.size 256 --set read-only "${finded_wallpapers[@]}" 2>/dev/null))
+
+    wal -n -i "${selected_wallpapers[0]}"
+
+    # Set wallpapers
+    feh --bg-fill "${selected_wallpapers[@]}"
+
+    echo "Is this color scheme/wallpaper fine? y/n"
+    read -n 1
+    [[ $REPLY =~ ^[yY]$ ]] && break
+done
 
 # Change colorscheme
 # custom_pywal.py "$1"
